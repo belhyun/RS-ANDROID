348905b05cb9dca0897c8da9d82ec7946ef252c2
yuseok <oysu@naver.com>
2014-03-31 16:17:19 +0900

    add main

--- SubWayGitTestProject/src/rs/subwaygittestproject/Main.java
+++ SubWayGitTestProject/src/rs/subwaygittestproject/Main.java
@@ -1,5 +1,28 @@
 package rs.subwaygittestproject;
 
-public class Main {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+public class Main extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.testlayout);
+
+		findViewById(R.id.button1).setOnClickListener(
+				new Button.OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						Toast.makeText(getBaseContext(), "!!",
+								Toast.LENGTH_SHORT).show();
+
+					}
+				});
+
+	}
 }
