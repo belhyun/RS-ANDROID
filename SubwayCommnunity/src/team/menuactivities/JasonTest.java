098a5dfe835e944d253217b2f0d18fc5312e933b
ohyuseok <ohyuseok@192.168.1.10>
2014-04-19 17:52:32 +0900

    size

--- SubwayCommnunity/src/team/menuactivities/JasonTest.java
+++ SubwayCommnunity/src/team/menuactivities/JasonTest.java
@@ -50,7 +50,7 @@ public class JasonTest extends Activity {
 
 			}
 		});
-//jkljkljk
-	}//
+
+	}
 
 }
--- SubwayCommnunity/src/team/subwaycommnunity/FragmentHome.java
+++ SubwayCommnunity/src/team/subwaycommnunity/FragmentHome.java
@@ -5,14 +5,89 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FragmentHome extends Fragment {
+
+	Button mHomeSubwayCommunityBtn;
+	Button mHomeEventWebBtn;
+	Button mHomeEasyTwitterBtn;
+	Button mHomeDeviceAppBtn;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		// 레이아웃 인플레이트
 		View v = inflater.inflate(R.layout.fragment_home, container, false);
+
 		return v;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		final View myView = getView();
+
+		// 버튼과 id 연결
+		mHomeSubwayCommunityBtn = (Button) myView
+				.findViewById(R.id.homeSubwayCommunityBtn);
+		mHomeEventWebBtn = (Button) myView.findViewById(R.id.homeEventWebBtn);
+		mHomeEasyTwitterBtn = (Button) myView
+				.findViewById(R.id.homeEasyTwitterBtn);
+		mHomeDeviceAppBtn = (Button) myView.findViewById(R.id.homeDeviceAppBtn);
+
+		myView.getViewTreeObserver().addOnGlobalLayoutListener(
+				new ViewTreeObserver.OnGlobalLayoutListener() {
+
+					@Override
+					public void onGlobalLayout() {
+
+						mHomeSubwayCommunityBtn.setHeight(myView.getHeight()*2/7);
+						mHomeEventWebBtn.setWidth(myView.getWidth()/3);
+						mHomeEventWebBtn.setHeight(myView.getHeight()*2/7);
+						mHomeEasyTwitterBtn.setWidth(myView.getWidth()/3);
+						mHomeDeviceAppBtn.setHeight(myView.getHeight()*2/7);
+					}
+				});
+
+		/* 버튼 리스너 */
+		mHomeSubwayCommunityBtn
+				.setOnClickListener(new Button.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						Toast.makeText(getActivity(), "?!", Toast.LENGTH_SHORT)
+								.show();
+					}
+				});
+		/* 버튼 리스너 */
+		mHomeEventWebBtn.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+		/* 버튼 리스너 */
+		mHomeEasyTwitterBtn.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+		/* 버튼 리스너 */
+		mHomeDeviceAppBtn.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
 	}
 
 }
